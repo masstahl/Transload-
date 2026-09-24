73abd4e60f1acdb9a7a6e50c1fda85b7fc3df5ad
masstahl <stahlsays@gmail.com>
2026-09-24 06:22:00 -0700

    Refresh compact mobile layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="transload-log-v4";
+const CACHE="transload-log-v5";
 const APP_ASSETS=["./","./index.html","./manifest.webmanifest","./apple-touch-icon.png"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
